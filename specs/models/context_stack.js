3204d11462b807cdaa02ce9ab7fe34ee607a8773
collin <collintmiller@gmail.com>
2008-10-19 17:47:48 -0500

    tests and generated files

---
+++ specs/models/context_stack.js
@@ -0,0 +1,10 @@
+describe('Sample', function() {
+  it("does equality", function() {
+    expect(0).to(equal, 1);
+  });
+ 
+  it("does equality", function() {
+    expect([1,2,4,66,6]).to(equal, [4,5,6]);
+  });
+});
+
