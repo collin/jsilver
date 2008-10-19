7f49aa63f59e2629273282be65912050dbf4fb32
collin <collintmiller@gmail.com>
2008-10-19 17:47:09 -0500

    more code for the logger

---
+++ specs/logger.js
@@ -0,0 +1,7 @@
+<style> .platform{width:32px;height:32px}.platform.ubuntu{background:url(/icons/ubuntu.png)}.platform.firefox{background:url(/icons/firefox.png)}.platform.msie{background:url(/icons/msie.png)}
+</style>
+
+
+
+
+
